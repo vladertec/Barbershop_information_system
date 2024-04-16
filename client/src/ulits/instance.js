bf8a8d892c299f47a12abb4af088906bd00ce2b4
vladertec <sarnavsk2001@gmail.com>
2024-04-16 12:50:05 +0200

    for deploying

--- client/src/ulits/instance.js
+++ client/src/ulits/instance.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
-const baseURL = "https://barbershop-information-systems-server.vercel.app"; 
+// const baseURL = "https://barbershop-information-systems-server.vercel.app"; 
+const baseURL = "http://data.advokat-sarnavskyi.com.ua"; 
 // const baseURL = "http://localhost:3000"; 
 
 const instance = axios.create({
